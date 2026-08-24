2745ea526bc688f6c080e020caa93942a1b55e22
Senya <sttephozol@gmail.com>
2026-08-24 10:32:09 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,25 @@
+#int
+age = 20
+pens = 3
+pen_eraser = 1
+notebooks = 7
+
+#float
+temp = 37.2
+weight = 45.5
+height = 157.5
+money = 100.25
+
+#string
+name = "Senya"
+address = "Market View"
+last_school_attended = "LCNHS"
+hobby = "Music"
+
+#Boolean
+SoloChild = False
+LowVision = True
+IsSick = True
+IsSmart = False
+
+print("The student's name is" , name, "and they are" , age, "years old. They have", pens,"pens," ,pen_eraser, "correction tape, and" ,notebook
