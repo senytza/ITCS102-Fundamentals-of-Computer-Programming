dff8c94f4832f3bb6761a1c9e31aa6065b561f82
Senya <sttephozol@gmail.com>
2026-08-26 11:29:21 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,21 @@
+#operators activity
+
+a = 7
+b = 5
+sum = a + b
+diff = a - b
+product = a * b
+dividend = a / b
+fdiv = a//b
+modulo = a%b
+exponent = a**b
+phrase1 = "The sum of a and b is",
+phrase2 = "The sum of", a," and", b,"is", sum
+phrase3 = "The difference of", a,"and", b,"is", diff
+phrase4 = "The product of", a,"and", b,"is",product
+phrase5 = "The dividend of", a,"and", b,"is", dividend
+phrase6 = "The floor division of", a,"and", b,"is", fdiv
+phrase7 = "The modulo of", a,"and", b,"is", modulo
+phrase8 = "The exponent of",a ,"and", b,"is", exponent
+
+print(phrase1)
