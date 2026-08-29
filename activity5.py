fe2afb3ea770769d29a8ff5f7b5f4c794b66c4bf
Senya <sttephozol@gmail.com>
2026-08-29 13:14:36 +0800

    Refactor arithmetic phrases and print statements
    Added prints for each arithmetic sequence.
    Cleaned things up a bit.

--- activity5.py
+++ activity5.py
@@ -6,16 +6,24 @@ sum = a + b
 diff = a - b
 product = a * b
 dividend = a / b
-fdiv = a//b
-modulo = a%b
-exponent = a**b
-phrase1 = "The sum of a and b is",
+fdiv = a // b
+modulo = a % b
+exponent = a ** b
+phrase1 = "The sum of a and b is", sum
 phrase2 = "The sum of", a," and", b,"is", sum
 phrase3 = "The difference of", a,"and", b,"is", diff
-phrase4 = "The product of", a,"and", b,"is",product
+phrase4 = "The product of", a,"and", b,"is", product
 phrase5 = "The dividend of", a,"and", b,"is", dividend
 phrase6 = "The floor division of", a,"and", b,"is", fdiv
-phrase7 = "The modulo of", a,"and", b,"is", modulo
+phrase7 = "The remainder of", a,"and", b,"is", modulo
 phrase8 = "The exponent of",a ,"and", b,"is", exponent
 
 print(phrase1)
+print("The sum of a and b is", sum)
+print("The sum of", a," and", b,"is", sum)
+print("The difference of", a,"and", b,"is", diff)
+print("The product of", a,"and", b,"is", product)
+print("The dividend of", a,"and", b,"is", dividend)
+print("The floor division of", a,"and", b,"is", fdiv)
+print("The remainder of", a,"and", b,"is", modulo)
+print("The exponent of",a ,"and", b,"is", exponent)
