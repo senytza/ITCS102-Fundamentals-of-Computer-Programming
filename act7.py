8d4d32617293e600a2cefc076781812ef7674d18
Senya <sttephozol@gmail.com>
2026-09-04 11:04:42 +0800

    Add files via upload

---
+++ act7.py
@@ -0,0 +1,26 @@
+#Activity7, Assignment Operation
+
+total = 2
+
+a = eval(input("Enter a number -->"))
+total += a
+b = eval(input("Enter a number -->"))
+total += b
+c = eval(input("Enter a number -->"))
+total -= c
+d = eval(input("Enter a number -->"))
+total *= d
+e = eval(input("Enter a number -->"))
+total += e
+f = eval(input("Enter a number -->"))
+total += f
+g = eval(input("Enter a number -->"))
+total += g
+h = eval(input("Enter a number -->"))
+total += h
+i = eval(input("Enter a number -->"))
+total += i
+j = eval(input("Enter a number -->"))
+total += j
+
+print ("Total is:", total)
