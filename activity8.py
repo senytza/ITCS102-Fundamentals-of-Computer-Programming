423c5ea78f19712cbc10a3679e992038dacf5b6b
Senya <sttephozol@gmail.com>
2026-09-04 11:28:59 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,26 @@
+#Activity8, Assignment Operation
+
+hobbies = ""
+
+a = input("What are your hobbies -->")
+hobbies += a + ", "
+b = input("What else -->")
+hobbies += b + ", "
+c = input("What else -->")
+hobbies += c + ", "
+d = input("What else -->")
+hobbies += d + ", "
+e = input("What else -->")
+hobbies += e + ", "
+f = input("What else -->")
+hobbies += f + ", "
+g = input("What else -->")
+hobbies += g + ", "
+h = input("What else -->")
+hobbies += h + ", "
+i = input("What else -->")
+hobbies += i + ", "
+j = input("What else -->")
+hobbies += j + ", "
+
+print ("My hobbies are", hobbies)
