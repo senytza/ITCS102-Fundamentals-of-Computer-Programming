dc0193338106615e7e8682c33d671c2f9a815152
Senya <sttephozol@gmail.com>
2026-09-16 13:58:50 +0800

    Refactor loan eligibility and interest rate logic

--- activity13.py
+++ activity13.py
@@ -35,85 +35,25 @@ print()
 print("-------------------------------------------------")
 print("--------------FINANCIAL EVALUATION---------------")
 print()
-if age >= 21 and is_employed:
+interest_rate = 0.0
+if age >= 21 and is_employed: #ELEGIBILITY
     print("You are eligible for a loan.")
-    if credit_score >= 750 :
-        interest_rate = "Approved at 5.0% interest."
+    if credit_score >= 750 : #TIER 1
         if annual_income >= 100000:
             interest_rate = "Approved at 4.5% interest."
-    if 600 <= credit_score <= 749:
+        else:
+            interest_rate = "Approved at 5.0% interest."
+    elif 600 <= credit_score <= 749: #TIER 2
         interest_rate = "Approved at 8.0% interest."
         if has_collateral:
             interest_rate = "Approved at 7.0% interest."
         if annual_income <= 39999:
             interest_rate = "Approved at 9.5 interest."
-    if credit_score <= 599:
+    elif credit_score <= 599: #TIER 3
         interest_rate = "Rejected: Credit Score Too Low"
     print(interest_rate)
 else:
     print("Rejected: Fails baseline criteria.")
-
-
-
-
-
-
-
-# #ACTIVITY FINISHED.
-# #age (integer)
-# #is_employed (boolean)
-# #credit_score (integer)
-# #annual_income (float)
-# #has_collateral (boolean)
-
-# #VARIABLES
-# age = int(input("Enter your age --> "))
-# is_employed = bool(input("Are you employed? Write 'True' if yes, leave blank if false --> "))
-# credit_score = int(input("Enter your credit score --> "))
-# annual_income = eval(input("Enter your annual income --> "))
-# has_collateral = bool(input("Do you have collateral? Write 'True' if yes, leave blank if false --> "))
-
-# #INPUTTED INFORMATION
-# print("-------------------------------------------------")
-# print("-------------INPUTTED INFORMATION----------------")
-# print()
-# print("User Age:",age)
-# print("Employment Status:",is_employed)
-# print("Credit Score:",credit_score)
-# print("Annual Income:",annual_income)
-# print("Collateral Status:",has_collateral)
-# print()
-# #CONDITIONS
-# print("-------------------------------------------------")
-# print("---------------LOAN ELIGIBILITY------------------")
-# print()
-# if age >= 21 and is_employed:
-#     print("You are eligible for a loan.")
-# else:
-#     print("Rejected: Fails baseline criteria.")
-# print()
-# print("-------------------------------------------------")
-# print("--------------FINANCIAL EVALUATION---------------")
-# print()
-# interest_rate = 0.0
-# if age >= 21 and is_employed: #ELEGIBILITY
-#     print("You are eligible for a loan.")
-#     if credit_score >= 750 : #TIER 1
-#         if annual_income >= 100000:
-#             interest_rate = "Approved at 4.5% interest."
-#         else:
-#             interest_rate = "Approved at 5.0% interest."
-#     elif 600 <= credit_score <= 749: #TIER 2
-#         interest_rate = "Approved at 8.0% interest."
-#         if has_collateral:
-#             interest_rate = "Approved at 7.0% interest."
-#         if annual_income <= 39999:
-#             interest_rate = "Approved at 9.5 interest."
-#     elif credit_score <= 599: #TIER 3
-#         interest_rate = "Rejected: Credit Score Too Low"
-#     print(interest_rate)
-# else:
-#     print("Rejected: Fails baseline criteria.")
 
 
 
